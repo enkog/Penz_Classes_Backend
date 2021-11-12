6fe0e1875c660b60d3ad7435ef145d437f432c8a
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-12 15:59:48 +0100

    Implement controller action to create new reservations

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ReservationsController < ApplicationController
+  def create
+    @reservation = Reservation.new(reservations_params)
+    if @reservation.save
+      render json: {
+        success: 'Reservation successful'
+      },
+             status: :created
+    else
+      render json: {
+        error: 'Reservation was not created'
+      },
+             status: :bad_request
+    end
+  end
+
+  private
+
+  def reservations_params
+    params.require(:reservation).permit(:user_id, :course_id, :start_date)
+  end
+end
