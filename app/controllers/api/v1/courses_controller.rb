81c452b5307074b2f0e1c75bbee156897c2aec78
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-12 15:25:43 +0100

    Add end to show

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
     render json: @course
+  end
 
   def index
     @courses = Course.all
