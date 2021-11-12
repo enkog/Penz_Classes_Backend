619d2165d86dab54b30cb1089e210675f5b79d4a
enkog <nkirukaogu@gmail.com>
2021-11-12 23:53:04 +0100

    add the session#create method for user authentication

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,29 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+  class AuthenticateError < StandardError; end
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  rescue_from AuthenticateError, with: :handle_unauthenticated
+
   def create
+    if user
+      session[:user_id] = user.id
+      raise AuthenticateError unless user
+      render json: UserSerializer.new(user).as_json, status: :created
+    else
+      render json: { error: 'User does not exist' }, status: :unauthorized
+    end
   end
 
-  def destroy
+  private
+
+  def user
+    @user ||= User.find_by(username: params[:username])
+  end
+
+  def parameter_missing(error)
+    render json: { error: error.message }, status: :unprocessable_entity
+  end
+
+  def handle_unauthenticated
+    render json: { error: 'Incorrect username ' }, status: :unauthorized
   end
 end
