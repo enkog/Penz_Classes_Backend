321ae341d37e8804d8d0baff49b795381d0fc810
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-12 15:16:35 +0100

    Implement action to create new courses

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::CoursesController < ApplicationController
-  def show
-    @test = 'hello'
-    render json: @test
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      render json: {
+        success: 'Course successfully created',
+      },
+      status: :created
+    else
+      render json: {
+        error: 'Course was not created',
+      },
+      status: :bad_request
+    end
+  end
+
+  private
+  
+  def course_params
+    params.require(:course).permit(:title, :description, :instructor_name, :image)
   end
 end
