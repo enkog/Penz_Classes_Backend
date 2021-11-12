7fb20d1981371dd4ad7d62e6390f202264527f88
enkog <nkirukaogu@gmail.com>
2021-11-12 11:25:20 +0100

    Add reservation validation and association

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  validates :user_id, :course_id, :start_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations
 
-  validates :username, length: { minimum: 2 }, presence: true, uniqueness: true
+  validates :username, length: { minimum: 3 }, presence: true, uniqueness: true
 end
