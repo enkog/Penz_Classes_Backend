19377359733ee4a0eb98285efe6d3bc70e56291e
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-12 15:22:13 +0100

    Fix linter errors

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -3,24 +3,24 @@ class Api::V1::CoursesController < ApplicationController
     @courses = Course.all
     render json: @courses
   end
-  
+
   def create
     @course = Course.new(course_params)
     if @course.save
       render json: {
-        success: 'Course successfully created',
+        success: 'Course successfully created'
       },
-      status: :created
+             status: :created
     else
       render json: {
-        error: 'Course was not created',
+        error: 'Course was not created'
       },
-      status: :bad_request
+             status: :bad_request
     end
   end
 
   private
-  
+
   def course_params
     params.require(:course).permit(:title, :description, :instructor_name, :image)
   end
