69c345af358c0ff7c3fb41d4845997f963423351
enkog <nkirukaogu@gmail.com>
2021-11-13 01:50:19 +0100

    Add controller action to create/register a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::UsersController < ApplicationController
     user = User.all
     render json: user
   end
+
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+
+    if user.save
+      render json: UserSerializer.new(user).as_json, status: :created
+    else
+      render json: { error: user.errors.full_messages.first }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
