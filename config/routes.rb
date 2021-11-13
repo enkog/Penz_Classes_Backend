6a36b2a0b7790bc827a53052cb87e265d666581b
enkog <nkirukaogu@gmail.com>
2021-11-13 01:46:28 +0100

    Define route to register a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :users, only: [:create, :destroy]
+      resources :users, only: :index
       resources :courses, only: [:index, :show, :create, :destroy]
       resources :reservations, only: [:index, :show, :create, :destroy]
       post 'login', to: 'session#create'
       delete 'logout', to: 'session#destroy'
+      post 'signup', to: 'users#create'
     end 
   end
 end
