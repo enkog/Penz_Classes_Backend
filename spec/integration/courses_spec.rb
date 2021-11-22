f6d00b75209d6258d4d91f170a294b47414afd5f
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-22 12:54:36 +0100

    Generate doc for course show action

--- spec/integration/courses_spec.rb
+++ spec/integration/courses_spec.rb
@@ -4,16 +4,45 @@ describe 'Courses API' do
 
   path '/api/v1/courses' do
 
-    get 'list courses' do
+    get 'List all courses' do
       tags 'Courses'
-      consumes 'application/json', 'application/xml'
+      produces 'application/json', 'application/xml'
 
-      response '200', :OK do
+      response '200', :success do
         let!(:course) { create(:course) }
         run_test!
       end
     end
   end
 
-  
+  path '/api/v1/courses/{id}' do
+
+    get 'Retrieves a course' do
+      tags 'Courses'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', :success do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            title: { type: :string },
+            description: { type: :string },
+            instructor_name: { type: :string },
+            image: { type: :string },
+            created_at: { type: :string },
+            updated_at: { type: :string }
+          },
+          required: [ 'id', 'title', 'description', 'instructor_name', 'image' ]
+
+        let(:id) { Course.create(:course).id }
+        run_test!
+      end
+
+      response '404', :not_found do
+        let(:id) { 'Couldn\'t find Course with given id' }
+        run_test!
+      end
+    end
+  end
 end
