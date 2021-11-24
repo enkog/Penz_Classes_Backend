34f62cf9511dfbfea839f708566ac9e47523be37
enkog <nkirukaogu@gmail.com>
2021-11-24 09:48:52 +0100

    Seed actual courses data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Course.destroy_all
 
-5.times do |count|
-  Course.create(title: Faker::Educator.subject, description: Faker::Lorem.characters, instructor_name: Faker::Name.unique.name, image: Faker::LoremFlickr.image)
-end
+Course.create(title: 'JavaScript', description: 'The course will teach you what are closures, IIFEs, Prototypal inheritances, etc. It will also help you to debug your project from your own home. You will be able to understand the source code of frameworks in JavaScript and build your own JavaScript library or framework.', instructor_name: 'James Smith', image: 'https://drive.google.com/file/d/1gO2lVDrB2GJP6LrEeWV5l76J54ZJVIa7/view?usp=sharing')
+
+Course.create(title: 'Node.js Masterclass', description: 'You need to master these platforms first before you can take a turn to become a professional Node developer. Node is a vast field and you can expect to face a lot of challenges. Courses on Node.js will help you jump over these obstacles and hone your skills in Node.js.', instructor_name: 'Anne Taylor', image: 'https://drive.google.com/file/d/1pg2Y4kCJnB5SeOfphcXzBYeICfBGIUhC/view?usp=sharing')
+
+Course.create(title: 'React and Redux', description: 'Redux and React are two major areas that any app developer should put their emphasis on. If you are someone who wants to make your own app or want to work for a company and build an app, then make Redux and React your friends.', instructor_name: 'Robert Williamson', image: 'https://drive.google.com/file/d/15xF_L1xgXGkFys8gJ0aia4RVmTFiWaUu/view?usp=sharing')
 
 3.times do
   User.create!(
